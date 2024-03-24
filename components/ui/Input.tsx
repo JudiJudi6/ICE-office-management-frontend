a6e1710c7969a9afa303c2e9312a6c05b1768bd7
Lukasz <7lukasz8@gmail.com>
2024-03-24 23:27:33 +0100

    input component

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,42 +1,58 @@
 "use client";
 
-import React, { Dispatch, SetStateAction, useState } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 interface InputProps {
-  label?: string;
+  label: string;
   setValue: Dispatch<SetStateAction<string | number>>;
-  placeholder: string;
   type: "text" | "email" | "number" | "password";
   id: string;
 }
 
-export default function Input({
-  placeholder,
-  setValue,
-  type,
-  id,
-  label,
-}: InputProps) {
+export default function Input({ setValue, type, id, label }: InputProps) {
   const [inputValue, setInputValue] = useState<string | number>("");
+  const [focus, setFocus] = useState(false);
 
   function changeValue(val: string | number) {
     setInputValue(val);
     setValue(val);
   }
 
+  useEffect(
+    function () {
+      if (inputValue) {
+        setFocus(true);
+      }
+    },
+    [inputValue, focus]
+  );
+
   return (
-    <div>
-      {label && (
-        <div>
-          <label htmlFor={id}>{label}</label>
-        </div>
-      )}
-      <input
+    <div className="relative">
+      <motion.label
+        htmlFor={id}
+        className="absolute top-1/2 left-3 text-gray cursor-text"
+        animate={
+          focus
+            ? { translateX: "-15px", translateY: "-140%", scale: 0.8 }
+            : { translateX: "0px", translateY: "-50%", scale: 1 }
+        }
+        initial={{ translateX: "0px", translateY: "-50%", scale: 1 }}
+        transition={{ ease: "linear", duration: 0.15 }}
+      >
+        {label}
+      </motion.label>
+      <motion.input
+        id={id}
+        className="border-none focus:outline-none px-3 py-2"
         type={type}
-        placeholder={placeholder}
         value={inputValue}
         onChange={(e) => changeValue(e.target.value)}
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
       />
+      <div className="absolute h-[1.5px] w-full bg-gradient-to-r from-main1 to-main2"></div>
     </div>
   );
 }
