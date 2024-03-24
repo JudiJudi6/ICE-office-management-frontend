1ce8814849ac618f9b96645f274a4dc1e8a69c8e
Lukasz <7lukasz8@gmail.com>
2024-03-24 23:30:08 +0100

    login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import Input from "@/components/ui/Input";
+import Logo from "@/components/ui/Logo";
 import TextLink from "@/components/ui/TextLink";
 import Link from "next/link";
-import React, { FormEvent, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 
 export default function LogIn() {
   const [email, setEmail] = useState<string | number>("");
@@ -14,38 +15,53 @@ export default function LogIn() {
     console.log(email, password);
   }
 
+  // useEffect(
+  //   function () {
+  //     console.log(email, password);
+  //   },
+  //   [email, password]
+  // );
+
   return (
-    <div className="bg-gradient-to-r from-orange-500 via-orange-400 to-yellow-400 text-bgDark">
-      <div className="bg-white">
-        <div>
-          <h3>Log in</h3>
-          <p>
-            Doesn&apos;t have an account yet?{" "}
-            <TextLink href="/signup">Sign Up</TextLink>
-          </p>
-          <form onSubmit={onSubmit}>
+    <div className="min-h-screen w-full h-full bg-gradient-to-br from-main3bg to-main4bg text-bgDark">
+      <div className="h-screen w-full bg-white p-6">
+        <div className="flex flex-col justify-center items-center  h-full ">
+          <div className="flex flex-col justify-center items-center mb-7">
+            <h3 className="font-semibold text-4xl">Hello there!</h3>
+            <p className="text-lg">Login to your account</p>
+          </div>
+          <form
+            onSubmit={onSubmit}
+            className="flex flex-col gap-4 md400:gap-6 md600:gap-8 xs:w-full xs:px-10 md500:w-[450px]"
+          >
             <div>
               <Input
                 id="email"
-                placeholder="Email"
                 setValue={setEmail}
                 type="email"
-                label="Enter email"
+                label="Email"
               />
             </div>
-            <div>
+            <div className="mb-6">
               <Input
                 id="password"
-                placeholder="Password"
                 setValue={setPassword}
                 type="password"
-                label="Enter password"
+                label="Password"
               />
             </div>
-            <button>submit</button>
+            <button className="w-full bg-gradient-to-r to-main1 via-main2 from-main1 bg-size-200 bg-pos-0 hover:bg-pos-100 py-2 text-white uppercase tracking-widest font-light rounded-full transition-all duration-300">
+              submit
+            </button>
           </form>
+          <p className="text-sm mt-4">
+            Doesn&apos;t have an account yet?
+            <br className="xs:hidden" />
+            <span className="xs:ml-2">
+              <TextLink href="/signup">Sign Up</TextLink>
+            </span>
+          </p>
         </div>
-        <div></div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center">
+    <div className="min-h-screen flex justify-center items-center">
       <h1>Home</h1>
       <Link href="/login">Log in</Link>
-    </main>
+    </div>
   );
 }
