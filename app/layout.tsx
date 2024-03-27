4f75249bc5fca98651f958f78460e03a421ca12b
Lukasz <7lukasz8@gmail.com>
2024-03-27 20:17:55 +0100

    nav changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Logo from "@/components/ui/Logo";
 import { FaFire } from "react-icons/fa";
 import LoginBtn from "@/components/features/nav/LoginBtn";
+import Link from "next/link";
+import BurgerNav from "@/components/features/nav/BurgerNav";
 
 const roboto = Roboto({
   weight: ["300", "400", "500", "700", "900"],
@@ -25,15 +27,16 @@ export default function RootLayout({
       <body className={roboto.className}>
         <div className="text-dark">
           <div className="fixed h-[64px] w-full bg-white border-b border-b-slate-200 flex justify-between items-center gap-3 px-4 py-2">
-            <div className="flex justify-start items-center gap-3">
+            <Link href="/" className="flex justify-start items-center gap-3">
               <span className="text-4xl text-main2">
                 <FaFire />
               </span>
               <p className="text-2xl tracking-wide">
                 Fire<span className="text-main2 font-medium">Desk</span>
               </p>
-            </div>
-            {/* <LoginBtn /> */}
+            </Link>
+            <LoginBtn />
+            <BurgerNav />
           </div>
           {children}
         </div>
