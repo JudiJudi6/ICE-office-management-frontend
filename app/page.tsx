ab55de2d1090a5f0f6d98a589817419beba6855a
Karol Nowak <karnowa88@gmail.com>
2024-04-12 21:14:28 +0200

    improved InfoBox - buttons, optional img etc

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
   const greetingDiv = (
     <div
       id="greeting"
-      className=" text-7xl items-center justify-center flex py-16 drop-shadow-md "
+      className=" z-0 text-7xl items-center justify-center flex py-16 drop-shadow-md "
     >
-      <h1 className="font-bold md800:flex ">
+      <h1 className="font-bold items-center lg:flex ">
         Welcome to
-        <span className="flex md800:ml-5 ">
+        <span className="flex lg:ml-5 ">
           <FaFire className="  text-main2 " />
           <span className="font-normal">Fire</span>
           <span className="text-main2">Desk</span>!
@@ -28,35 +28,54 @@ export default function Home() {
         Book your desk and do not worry about place to work!
       </h1>
 
-      <a href="/login" className="flex justify-center">
-        <button className=" m-4  w-40 h-10  shadow-md bg-gradient-to-r to-main1 via-main2 from-main1 bg-size-200 bg-pos-0 hover:bg-pos-100 py-2 text-white uppercase tracking-widest font-light rounded-full transition-all duration-300">
+      <div className="flex justify-center">
+        <button className="  m-4  w-40 h-10  shadow-md bg-gradient-to-r to-main1 via-main2 from-main1 bg-size-200 bg-pos-0 hover:bg-pos-100 py-2 text-white uppercase tracking-widest font-light rounded-full transition-all duration-300">
+        <a href="/login">
           Log in
+          </a>
         </button>
-      </a>
+        </div>
+      
     </div>
   );
 
   return (
-    <div className="min-h-screen py-20  ">
+    <div className=" z-0 min-h-screen py-20 overflow-hidden  ">
       {greetingDiv}
       {subtitle}
       <br />
-      <div className=" xl:grid  grid-cols-2 justify-center mx-20 my-8 px-5 ">
+      <div className="   justify-center mx-10 my-8 px-5 ">
+        <div className="lg:flex flex-row items-center">
+        <div className="basis-2/3 ">
         <InfoBox
           boxTitle="What is FireDesk?"
+          text="FireDesk is a tool that allows you to book your place to work. Only thing you need is a FireDesk account."
+          srcImg="/images/image2.png"
+          showButton={false}
+          showImg={true}
+        />
+        </div>
+        <div className="basis-1/3  h-full">
+        <InfoBox
+        boxTitle="New to FireDesk?"
+          text={"Lorem, ipsum dolor sit amet consectetur adipisicing elit. "}
+          showButton={true}
+          hrefButton="/signup"
+          titleButton="Sign up now!"
+          showImg={false}
+
+        />
+        </div>
+        </div>
+        <div className="basis-full">
+        <InfoBox 
+          boxTitle="New to FireDesk?"
           text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic atque veniam nisi iure molestias voluptate fuga deleniti ipsa aperiam."
-          image="/images/image.png"
+          srcImg="/images/office.jpg"
+          showImg={true}
+          showButton={false}
         />
-        <InfoBox
-          boxTitle="New to FireDesk?"
-          text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic atque veniam nisi iure molestias voluptate fuga deleniti ipsa aperiam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic atque veniam nisi iure molestias voluptate fuga deleniti ipsa aperiam."
-          image="/images/office.jpg"
-        />
-        <InfoBox
-          boxTitle="New to FireDesk?"
-          text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic atque veniam nisi iure molestias voluptate fuga deleniti ipsa aperiam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic atque veniam nisi iure molestias voluptate fuga deleniti ipsa aperiam."
-          image="/images/office.jpg"
-        />
+        </div>
       </div>
     </div>
   );
--- components/ui/InfoBox.tsx
+++ components/ui/InfoBox.tsx
@@ -1,31 +1,68 @@
 import Image from "next/image";
 //import zdj from "/image.png";
 
-export default function InfoBox({ boxTitle, text, image }: BoxTitle) {
-  const ratio = 20;
+export default function InfoBox({ boxTitle, text,showButton, hrefButton, titleButton,showImg, srcImg }: boxText) {
 
-  return (
-    <div className=" w-full shadow-xl my-8 mx-4 p-1 rounded-3xl flex ">
-      <div className="md:flex justify-center">
-        <div className=" p-8 bg-gradient-to-r to-main1 via-main2 from-main1 bg-size-200 bg-pos-0 hover:bg-pos-100 py-2 text-white  tracking-widest font-light transition-all duration-300 rounded-3xl">
+  const image = srcImg || ""
+
+
+
+
+const isButton=( showButton ?
+  <div className="  md:w-full md:h-full p-8 py-2 text-white text-center font-light  ">
           <h1 className="font-bold text-3xl">{boxTitle}</h1>
           <p className="text-1xl">{text}</p>
-        </div>
-        <Image
-          src={image}
-          alt={image}
-          width={1000}
-          height={1000}
-          layout="responsive"
-          className="rounded-3xl mx-1 max-w-64 w-auto h-auto"
-        />
+          <div className=" flex md:grid grid-cols-1 justify-center">
+             <button className="my-3 p-2 text-main1 bg-white rounded-full">
+             <a href={hrefButton}>{titleButton}</a></button> <></>
+          </div>
+          </div>
+          :
+          <div className="  md:w-full md:h-full p-8 py-2  text-white font-light  ">
+          <h1 className="font-bold text-3xl">{boxTitle}</h1>
+          <p className="text-1xl">{text}</p>
+          
+          </div>
+
+);
+
+const isImg=( showImg ?<div className=" flex justify-self-end place-content-center  ">
+<Image
+  src={image}
+  alt={image}
+  width={1000}
+  height={1000}
+  
+  className=" md:aspect-auto rounded-3xl my-1 md:mx-2 md:my-0 max-w-80 max-h-64 w-auto h-auto"
+/>  
+</div>
+  :<></>
+);
+
+  return (
+    <div className=" mx-2 shadow-xl my-3 xl:mx-3 rounded-3xl bg-gradient-to-r to-main1 via-main2 from-main1 bg-size-200 bg-pos-0 hover:bg-pos-100 py-2  tracking-widest font-light transition-all duration-300">
+      <div className="md:flex w-full h-full grid-cols-2   items-center  ">
+        
+        {isButton}
+        {isImg}
+       
+        
       </div>
     </div>
   );
 }
 
-interface BoxTitle {
+interface boxText {
   boxTitle: string;
   text: string;
-  image: string;
+  showButton: boolean;
+  hrefButton?: string;
+  titleButton?: string;
+  showImg: boolean;
+  srcImg?: string;
+ 
+}
+
+InfoBox.defaultProps ={
+
 }
