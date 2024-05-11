7c4f5989c0659ea75f0c401b49305c79e16f2860
Lukasz <7lukasz8@gmail.com>
2024-05-11 17:33:34 +0200

    new functions to fetching reservations

--- services/apiOffices.ts
+++ services/apiOffices.ts
@@ -38,8 +38,25 @@ export async function getOffice() {
   }
 }
 
-export async function getUserOffice(userId: string | null) {
+export async function getUserOffice(userId: string | undefined) {
   const response = await fetch(API_KEY + "/api/v1/user/" + userId, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  } else {
+    const bodyText = await response.text();
+    console.error(response);
+    throw new Error(`${bodyText}`);
+  }
+}
+
+export async function getDeskReservations(deskId: string, officeId: string | undefined) {
+  const response = await fetch(API_KEY + "/api/v1/reservations/" + officeId + "/" + deskId, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
